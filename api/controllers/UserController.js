5ccf026bfac4d521dea06bf129e633dd6564631e
PedroLopes7K <pedrohenri3601@gmail.com>
2022-10-26 19:34:58 -0300

    finished crud

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -10,6 +10,74 @@ class UserController {
       return res.status(500).json(error.message)
     }
   }
+
+  static async getUser(req, res) {
+    const { id } = req.params
+    try {
+      const user = await database.Users.findOne({
+        where: {
+          id: Number(id)
+        }
+      })
+      return res.status(200).json(user)
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async insertUser(req, res) {
+    const userData = {
+      name: req.body.name,
+      active: req.body.active,
+      email: req.body.email,
+      role: req.body.role
+    }
+    try {
+      const user = await database.Users.create(userData)
+      return res
+        .status(200)
+        .json({ msg: 'User updated successfuly!', user: user })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async updatedUser(req, res) {
+    const { id } = req.params
+    const infosUpdated = req.body
+    try {
+      await database.Users.update(infosUpdated, {
+        where: {
+          id: Number(id)
+        }
+      })
+
+      const userUpdated = await database.Users.findOne({
+        where: {
+          id: Number(id)
+        }
+      })
+      return res
+        .status(200)
+        .json({ msg: 'User updated successfuly!', user: userUpdated })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async deleteUser(req, res) {
+    const { id } = req.params
+    try {
+      await database.Users.destroy({
+        where: {
+          id: Number(id)
+        }
+      })
+      return res.status(200).json({ msg: 'User deleted successfuly!' })
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
 }
 
 module.exports = UserController
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -4,5 +4,9 @@ const UserController = require('../controllers/UserController')
 const router = Router()
 
 router.get('/users', UserController.getAllUsers)
+router.get('/users/:id', UserController.getUser)
+router.post('/users', UserController.insertUser)
+router.put('/users/:id', UserController.updatedUser)
+router.delete('/users/:id', UserController.deleteUser)
 
 module.exports = router
