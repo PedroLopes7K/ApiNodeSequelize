bc1fd7df9c5bdec0c07a52248774906a1afa9214
PedroLopes7K <pedrohenri3601@gmail.com>
2022-10-27 18:35:04 -0300

    delete route of test

--- api/index.js
+++ api/index.js
@@ -8,10 +8,6 @@ const port = 3000
 // app.use(bodyParser.json())
 routes(app)
 
-// app.get('/teste', (req, res) => {
-//   res.status(200).send({ msg: 'Ok' })
-// })
-
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 })
